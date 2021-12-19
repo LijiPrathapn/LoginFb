cc6f80212d67ef5b9ac8fd1e32baa2c2b567e63b
LijiPrathapn <63522072+LijiPrathapn@users.noreply.github.com>
2021-12-19 11:59:24 -0800

    Update run1.bat

--- run1.bat
+++ run1.bat
@@ -1,2 +1,2 @@
-call C:\Users\lijin\PycharmProjects\LoginFb>C:\Users\lijin\AppData\Local\Programs\Python\Python37\Scripts\pytest.exe  -v -s Tests\test_login.py
-rem pytest -v -s Tests\test_login.py
+rem call C:\Users\lijin\PycharmProjects\LoginFb>C:\Users\lijin\AppData\Local\Programs\Python\Python37\Scripts\pytest.exe  -v -s Tests\test_login.py
+pytest -v -s Tests\test_login.py
